b48be83a32cb1befe908ced7c4d680a5a9ba23fc
Wen Zhao (Joan) <withzhaowen@126.com>
2022-06-25 23:59:19 +0800

    update

--- jpa-demo1/src/main/java/com/github/ybqdren/JpaApplication.java
+++ jpa-demo1/src/main/java/com/github/ybqdren/JpaApplication.java
@@ -1,10 +1,16 @@
 package com.github.ybqdren;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zhao wen
  * @sice 2022/6/25
  **/
 
-
+@SpringBootApplication
 public class JpaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class,args);
+    }
 }
